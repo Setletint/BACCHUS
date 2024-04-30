407bf86f4f61407fd837c8d57e3022cc0aa49258
Setletint <polikq772@gmail.com>
2024-04-30 10:23:06 +0300

    Created Bid methods. Added Body-parser

--- expressBackend/app.js
+++ expressBackend/app.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const bodyParser = require('body-parser')
 const express = require('express')
 const Bid = require('./func/Bid')
 const app = express()
@@ -7,19 +8,38 @@ const auctionUrl = 'http://uptime-auction-api.azurewebsites.net/api/Auction';
 
 let auctions = new Array();
 
-
+var jsonParser = bodyParser.json()
+ 
 app.get('/auctions', (req, res) => {
     axios.get('http://uptime-auction-api.azurewebsites.net/api/Auction')
         .then((response) => {
             auctions = response.data;
+
+            let sem = new Bid
+            console.log(sem.checkBid('DASDA31231'))
+
             res.send(auctions);
         }).catch((error) => {
             res.status(500).send('External server is disabled')
         })
 })
 
-app.post('/auctions',(req,res)=>{
-    console.log(req);
+app.post('/makebid', jsonParser ,(req,res)=>{
+    console.log(req.body);
+    let userName   = req.body.name;
+    let userSurname= req.body.surname;
+    let auctionID  = req.body.auctionID;
+    let currentTimeDate = new Date;
+
+    if(userName && userSurname){
+        userName = userName.replace(/\s/g, '');
+        userSurname = userSurname.replace(/\s/g, '');
+        
+        let bidID = userName + userSurname + currentTimeDate.toJSON();
+        console.log(bidID);
+    }
+
+
     res.send('');
 })
 
--- expressBackend/func/Bid.js
+++ expressBackend/func/Bid.js
@@ -2,7 +2,36 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./expressBackend/bidsDatabase.sqlite');
 
 class Bid{
-    makeBid(productId,userName,userFullname,amount) {
+    createDatabase(){
+        db.serialize(()=>{
+            db.run(`CREATE TABLE bids(
+                auctionID varchar(255),
+                userID varchar(255),
+                highestBid DECIMAL(255,2)
+                );`)
+        })
+    }
+    dropBidsTable(){
+        db.serialize(()=>{
+            db.run("DROP TABLE bids")
+        })
+    }
+
+    checkBid(productId,userID,amount) {
+        db.serialize(()=>{
+            db.get(`SELECT MAX(highestBid) FROM bids WHERE auctionID='`+productId+`'`, (err,row)=>{
+                if(err){
+                    return console.error(err.message);
+                }
+                return row.highestBid
+            })
+        })
+    }
+
+    makeBid(productId,userID,amount){
+        db.serialize(()=>{
+            db.run(`INSERT INTO bids(auctionID, userID, highestBid) VALUES ('`+productId+`','`+userID+`','`+amount+`')`)
+        })
     }
 }
 

abb189f1678275f95e2a89f2ab926937e6042696
Setletint <72040591+Setletint@users.noreply.github.com>
2024-05-07 09:34:06 +0300

    Made bid exist check

--- expressBackend/app.js
+++ expressBackend/app.js
@@ -20,32 +20,52 @@ app.get('/auctions', (req, res) => {
         })
 })
 
+app.get('/getBid/:id', (req, res) => {
+    let reqId = req.params['id']
+    if (reqId != undefined) {
+        let sem = new Bid
+        let dbRes = sem.checkBid(reqId)
+        console.log(dbRes);
+        if(dbRes != null){
+            res.send(dbRes)
+        }else{
+            res.status(404).send('bid not found')
+        }
+    }else{
+        res.status(400).send('no parameters were given')
+    }
+})
+
 app.post('/makebid', jsonParser, (req, res) => {
-    console.log(req.body);
+
+    let isBidExist = false
     let userName = req.body.name;
     let userSurname = req.body.surname;
     let auctionID = req.body.auctionID;
+    let auctionName = req.body.auctionName;
     let amount = req.body.amount;
     let currentTimeDate = new Date;
 
     if (userName && userSurname) {
         userName = userName.replace(/\s/g, '');
         userSurname = userSurname.replace(/\s/g, '');
+        userName = userName.replaceAll(';','')
+        userSurname = userSurname.replaceAll(';', '');
 
-        let bidID = userName + userSurname + currentTimeDate.toJSON();
+        let bidID = userName + ';' + userSurname +';'+ currentTimeDate.toJSON();
         console.log(bidID);
 
         let sem = new Bid
         highiestBid = sem.checkBid(auctionID);
 
+        if(highiestBid == null) isBidExist = true;
+
         if (amount > highiestBid) {
-            sem.makeBid(auctionID, bidID, amount)
+            sem.makeBid(auctionID,auctionName ,bidID, amount, isBidExist)
+            res.status(201).send('bid has been created')
         }
     }
-
-
-
-    res.send('');
+    res.status(200).send('');
 })
 
 app.listen(port, () => {
--- expressBackend/func/Bid.js
+++ expressBackend/func/Bid.js
@@ -2,16 +2,17 @@ const Database = require('better-sqlite3');
 const db = new Database('./expressBackend/bidsDatabase.sqlite');
 db.pragma('journal_mode = WAL');
 
-class Bid{
-    createDatabase(){
+class Bid {
+    createDatabase() {
         db.exec(`CREATE TABLE bids(
             auctionID varchar(255),
+            auctionName varchar(255),
             userID varchar(255),
             highestBid DECIMAL(255,2)
             );`)
-        
+
     }
-    dropBidsTable(){
+    dropBidsTable() {
         db.exec("DROP TABLE bids")
     }
 
@@ -21,13 +22,15 @@ class Bid{
         return result[0][1];
     }
 
-    makeBid(productId,userID,amount){
-        const stmt = db.prepare(`INSERT INTO bids(auctionID, userID, highestBid) VALUES (?,?,?)`)
-        const inform = stmt.run(productId,userID,amount)
+    makeBid(productId, auctionName, userID, amount, isBidExist) {
+
+        if (isBidExist == false) {
+            const stmt = db.prepare(`INSERT INTO bids(auctionID, auctionName, userID, highestBid) VALUES (?,?,?,?)`)
+            const inform = stmt.run(productId, auctionName, userID, amount)
+        }else{
+            const stmt = db.prepare(`UPDATE bids SET userID = ?, highestBid = ? WHERE auctionID = ?`)
+            const inform = stmt.run(userID, amount, productId)
+        }
     }
 }
-
-let das = new Bid
-das.makeBid('DASDAS123','SomeUID',254)
-
 module.exports = Bid;
